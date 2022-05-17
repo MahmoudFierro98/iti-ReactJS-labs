f405138b0f8106b263c825ddbeedab5ba7365465
Mahmoud Mohamed Kamal <54429800+MahmoudFierro98@users.noreply.github.com>
2022-05-17 20:10:36 +0200

    Add Lab 2

---
+++ Lab2/lab2/src/routing/About.js
@@ -0,0 +1,4 @@
+const About = () => {
+    return (<h3 style={{ backgroundColor: "black", color: "white" }}><marquee>Created by: <a style={{ color: "blue" }} href="https://www.linkedin.com/in/mahmoudfierro98/" target='_blank'>Mahmoud Mohamed Kamal</a></marquee></h3>);
+}
+export default About;
